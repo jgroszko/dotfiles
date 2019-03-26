80141243b292e61a24976454564a9cdf9af04b43
John Groszko <john@tinythunk.com>
2019-03-25 21:53:48 -0500

    Color tweaks

--- git-prompt.sh
+++ git-prompt.sh
@@ -1,28 +1,30 @@
 # The various escape codes that we can use to color our prompt.
 FG_YELLOW="\[\e[38;5;226m\]"
-FG_GREY_0="\[\e[38;5;234m\]"
-FG_GREY_1="\[\e[38;5;236m\]"
-FG_GREY_2="\[\e[38;5;238m\]"
-FG_GREY_3="\[\e[38;5;255m\]"
+FG_GREY_0="\[\e[38;5;235m\]"
+FG_GREY_1="\[\e[38;5;237m\]"
+FG_GREY_2="\[\e[38;5;239m\]"
+FG_GREY_3="\[\e[38;5;245m\]"
+FG_GREY_4="\[\e[38;5;255m\]"
 
 BG_BLACK="\[\e[48;5;0m\]"
-BG_GREY_0="\[\e[48;5;234m\]"
-BG_GREY_1="\[\e[48;5;236m\]"
-BG_GREY_2="\[\e[48;5;238m\]"
-BG_GREY_3="\[\e[48;5;255m\]"
+BG_GREY_0="\[\e[48;5;235m\]"
+BG_GREY_1="\[\e[48;5;237m\]"
+BG_GREY_2="\[\e[48;5;239m\]"
+BG_GREY_3="\[\e[48;5;245m\]"
+BG_GREY_4="\[\e[48;5;255m\]"
 
 CLEAN_COLOR=$FG_GREY_3
 DIRTY_COLOR=$FG_YELLOW
 
 ARROW=""
 
-PRE_ARROW="$BG_GREY_2$FG_GREY_3"
-ARROW_FIRST="$FG_GREY_2$BG_GREY_1$ARROW$FG_GREY_3"
+PRE_ARROW="$BG_GREY_2$FG_GREY_4"
+ARROW_FIRST="$FG_GREY_2$BG_GREY_1$ARROW$FG_GREY_4"
 
-ARROW_SECOND_FINAL="$FG_GREY_1$BG_BLACK$ARROW$FG_GREY_3"
+ARROW_SECOND_FINAL="$FG_GREY_1$BG_BLACK$ARROW$FG_GREY_4"
 
 ARROW_SECOND="$FG_GREY_1$BG_GREY_0$ARROW$FG_GREY_3"
-ARROW_THIRD="$FG_GREY_0$BG_BLACK$ARROW$FG_GREY_3"
+ARROW_THIRD="$FG_GREY_0$BG_BLACK$ARROW$FG_GREY_4"
 
 # Detect whether the current directory is a git repository.
 function is_git_repository {
