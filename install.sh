673107c067c7f44d0028518ac27b4e729b92f643
John Groszko <john@tinythunk.com>
2019-04-05 23:48:28 -0500

    tmux - Templates & remote conf

--- install.sh
+++ install.sh
@@ -12,8 +12,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # tmux conf
 #
 
+pipenv run jinja2 tmux.conf.tmpl colors.local.json > tmux.conf
 if [ ! -e ~/.tmux.conf ]; then
 	ln -s $DIR/tmux.conf ~/.tmux.conf
+fi
+
+pipenv run jinja2 tmux.conf.tmpl colors.remote.json > tmux.conf.remote
+if [ ! -e ~/.tmux.conf.remote ]; then
+	ln -s $DIR/tmux.conf.remote ~/.tmux.conf.remote
 fi
 
 if [ ! -e ~/bin/git-status.sh ]; then
--- profile.sh
+++ profile.sh
@@ -3,3 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/git-prompt.sh
+
+function sst () {
+	/usr/bin/ssh \
+		-o PermitLocalCommand=yes \
+		-o LocalCommand="cat ${HOME}/.tmux.conf.remote | ssh -o PermitLocalCommand=no %n \"cat > ${HOME}/.tmux.conf\"" \
+		-t $@ "tmux new-session -A -s \"`hostname -fs`\""
+}
