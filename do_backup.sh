e62b46f7345dc0017ec97667f8e69847c3112ab9
John Groszko <john@tinythunk.com>
2020-05-23 11:17:03 -0500

    Add do_backup.sh

---
+++ do_backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BACKUP_HOST="lirael"
+BACKUP_USER="john"
+BACKUP_SSH_KEY="/home/john/.ssh/id_rsa"
+BACKUP_PATH="/volume1/backup/homes/$(hostname)/daily"
+
+resolvedIp=$(nslookup "$BACKUP_HOST" | awk -F':' '/^Address: / { matched = 1 } matched { print $2 }' | xargs)
+
+echo Got ip "$resolvedIp"
+
+if [ -n "$resolvedIp" ]
+then
+	rsync -avz \
+		  --delete \
+		  --append-verify \
+		  --partial \
+		  --progress \
+		  -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /home/john/.ssh/id_rsa" \
+		  --exclude ".cache" \
+		  --exclude "*.iso" \
+		  --exclude "*.ova" \
+		  --exclude "node_modules" \
+		  --exclude ".local/share/virtualenvs" \
+		  --exclude "/.snapshots" \
+		  --exclude "Dropbox" \
+		  --exclude "VirtualBox VMs" \
+		  /home/ "$BACKUP_USER"@"$BACKUP_HOST":"$BACKUP_PATH"
+fi
