29a068ffb9e1212eb4b248099374a4e0b74d0bd3
John Groszko <john@tinythunk.com>
2020-07-05 10:36:13 -0500

    Add yarn/npm aliases

--- profile.sh
+++ profile.sh
@@ -10,3 +10,6 @@ function sst () {
 		-o LocalCommand="cat ${HOME}/.tmux.conf.remote | ssh -o PermitLocalCommand=no %n \"cat > ${HOME}/.tmux.conf\"" \
 		-t $@ "tmux new-session -A -s \"`hostname -fs`\""
 }
+
+alias rmrfnm='rm -rf node_modules && npm install'
+alias rfrfym='rm -rf node_modules && yarn install'
