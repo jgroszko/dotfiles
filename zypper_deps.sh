7cb3e6da5f9e5794a1c36466a6f2cc407f871118
John Groszko <john@tinythunk.com>
2022-06-19 14:31:42 -0500

    Update for LEAP 15.4

--- zypper_deps.sh
+++ zypper_deps.sh
@@ -7,6 +7,14 @@ sudo mkfs.ext4 /mnt/docker-volume.img
 sudo -c 'echo "/mnt/docker-volume.img /var/lib/docker ext4 defaults 0 0" >> /etc/fstab'
 sudo mount /var/lib/docker
 
+# User Groups
+sudo usermod -a -G dialout,docker $USER
+
+# udev rules
+sudo cp ./dev/dotfiles/udev/*.rules /etc/udev/rules.d
+sudo cp ./dev/dotfiles/udev/*.sh /usr/local/bin
+
+# Extra Package Repos
 sudo rpm --import https://dl.google.com/linux/linux_signing_key.pub
 sudo zypper ar https://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
 
@@ -14,9 +22,9 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo zypper ar https://packages.microsoft.com/yumrepos/vscode vscode
 
 sudo rpm --import http://ftp.gwdg.de/pub/linux/misc/packman/public-keys.asc
-sudo zypper ar http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_15.2/ Packman
+sudo zypper ar http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_15.4/ Packman
 
-sudo zypper ar https://download.opensuse.org/repositories/graphics/openSUSE_Leap_15.2/ graphics
+sudo zypper ar https://download.opensuse.org/repositories/graphics/15.4/ graphics
 
 sudo zypper refresh
 sudo zypper in \
@@ -25,7 +33,6 @@ sudo zypper in \
     git \
     emacs-nox \
     tmux \
-    blender \
     gimp \
     inkscape \
     code \
@@ -39,7 +46,3 @@ sudo zypper in \
     vlc-codecs \
     exfat-utils \
 	jq sqlite3 curl bind-utils
-
-# udev rules
-sudo cp ./dev/dotfiles/udev/*.rules /etc/udev/rules.d
-sudo cp ./dev/dotfiles/udev/*.sh /usr/local/bin
